bf619653279e27e11f74b89c57d318782fc24283
ghani <agaarbi@gmail.com>
2022-09-16 13:25:46 +0500

    hashed password

--- mongodb/accounts/views.py
+++ mongodb/accounts/views.py
@@ -1,15 +1,17 @@
+from django.conf import settings
+import pymongo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+import bcrypt
 
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 
-import pymongo
-# connect_string = 'mongodb+srv://<username>:<password>@<atlas cluster>/<myFirstDatabase>?retryWrites=true&w=majority' 
+# connect_string = 'mongodb+srv://<username>:<password>@<atlas cluster>/<myFirstDatabase>?retryWrites=true&w=majority'
 connect_string = config["connect_string"]
-from django.conf import settings
 my_client = pymongo.MongoClient(connect_string)
 # First define the database name
 dbname = my_client['user_accounts']
@@ -20,16 +22,20 @@ collection_name = dbname["accounts"]
 # Create your views here.
 @api_view(['POST'])
 def create_user(request):
-    mongo_write(request.data)
-    print(request.data)
-    return Response({"status" : "user registered"})
+    password = request.data['password']
+    password = password.encode('utf-8')
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    user = {
+        "first_name": request.data['first_name'],
+        "last_name": request.data['last_name'],
+        "username": request.data['username'],
+        "password": hashed,
+        "email": request.data['email']
+    }
+    collection_name.insert_one(user)
+    print(user)
+    return Response({"status": "user registered"})
     # return Response({"invalid": "criteria not match"})
-
-
-def mongo_write(data):
-    account_info = data
-    # Insert the documents
-    collection_name.insert_one(account_info)
 
 
 """{
